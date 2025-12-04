f6be48a9c5dac463a4d32e3bb182427a928777ac
abeerseada <121635964+abeerseada@users.noreply.github.com>
2025-12-04 02:15:19 +0200

    dockerfile path updated

--- ci/steps/buildImage.groovy
+++ ci/steps/buildImage.groovy
@@ -1,7 +1,7 @@
 def call() {
     stage('Build Image ') {
         echo ' Building Docker image...'
-        sh """docker build -t abeerseada/flask-app-1:$BUILD_NUMBER ."""
+        sh """docker build -t abeerseada/flask-app-1:$BUILD_NUMBER ./app/."""
 		echo ' Docker image built successfully.'
 		currentBuild.displayName = "#${BUILD_NUMBER} - myimg"
 		currentBuild.description = "Built Docker image for myimg"
