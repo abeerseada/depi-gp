28c1b4a150aaf6f6695fc6e465475b2ce93c4d62
abeerseada <121635964+abeerseada@users.noreply.github.com>
2025-12-04 02:19:34 +0200

    img multiplatform

--- ci/steps/buildImage.groovy
+++ ci/steps/buildImage.groovy
@@ -1,7 +1,7 @@
 def call() {
     stage('Build Image ') {
         echo ' Building Docker image...'
-        sh """docker build -t abeerseada/flask-app-1:$BUILD_NUMBER ./app/."""
+        sh """docker buildx build --platform linux/amd64,linux/arm64  -t abeerseada/flask-app-1:$BUILD_NUMBER ./app/."""
 		echo ' Docker image built successfully.'
 		currentBuild.displayName = "#${BUILD_NUMBER} - myimg"
 		currentBuild.description = "Built Docker image for myimg"
