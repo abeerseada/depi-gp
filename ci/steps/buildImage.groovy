53af68f71c71d142963b588a4ac7034d3f9fb268
abeerseada <121635964+abeerseada@users.noreply.github.com>
2025-12-09 05:54:07 +0200

    jenkins build img configured

--- ci/steps/buildImage.groovy
+++ ci/steps/buildImage.groovy
@@ -1,13 +1,18 @@
 def call() {
-    stage('Build Image ') {
-        echo ' Building Docker image...'
-        sh """docker buildx build \
-            --platform linux/amd64 \            
-            -t abeerseada/flask-app-1:$BUILD_NUMBER  \
-            --push ./app/."""
-		echo ' Docker image built successfully.'
-		currentBuild.displayName = "#${BUILD_NUMBER} - myimg"
-		currentBuild.description = "Built Docker image for myimg"
+    stage('Build Image') {
+        echo 'Building Docker image...'
+
+        sh """
+            docker buildx build \
+            --platform linux/amd64 \
+            -t abeerseada/flask-app-1:${BUILD_NUMBER} \
+            --push ./app/.
+        """
+
+        echo 'Docker image built successfully.'
+        currentBuild.displayName = "#${BUILD_NUMBER} - myimg"
+        currentBuild.description = "Built Docker image for myimg"
     }
 }
+
 return this
